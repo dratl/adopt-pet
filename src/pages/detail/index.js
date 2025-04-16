32667e87ec8e93df254240f6c4c502359a1c8e58
Xico Trujano <xtrujano@mac.com>
2025-04-16 01:45:01 -0600

    No details page (404)

--- src/pages/detail/index.js
+++ src/pages/detail/index.js
@@ -3,7 +3,7 @@ import { getPetDetails } from '../../api/petfinder';
 import Hero from '../../components/hero';
 
 // Import useParams
-import { useParams } from 'react-router-dom';
+import { useParams, Navigate } from 'react-router-dom';
 // Import Navigate
 
 const PetDetailsPage = () => {
@@ -33,7 +33,7 @@ const PetDetailsPage = () => {
         <h3>Loading...</h3>
       ) : error ? (
         <div>
-          {/* Redirect to /pet-details-not-found if there was an error! */}
+          <Navigate to="/pet-details-not-found" />
         </div>
       ) : (
         <main>
